eaa6f37f0f6815706fa20c1a6884aafaa4bf0712
Katarzyna Wietecha-Mrzyglod <k.wietecha-mrzyglod@interactivevision.pl>
2019-01-28 12:27:24 +0100

    Sign out

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    Dashboard component
+    <h2>My events</h2>
+    <button class="btn btn-danger signout-btn" @click="signOut">Sign out</button>
     <p>{{ $store.state }}</p>
   </div>
 </template>
+
+
+<script>
+import { firebaseApp } from "../firebaseApp"
+
+export default {
+  methods: {
+    signOut(){
+      this.$store.dispatch("signOut")
+      firebaseApp.auth().signOut()
+    }
+  }
+}
+
+</script>

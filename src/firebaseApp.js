701b1b0b3620c6342e1c5456990babcd13552533
Katarzyna Wietecha-Mrzyglod <k.wietecha-mrzyglod@interactivevision.pl>
2019-02-04 13:57:39 +0100

    Fixed warning about firebase

--- src/firebaseApp.js
+++ src/firebaseApp.js
@@ -1,4 +1,7 @@
-import firebase from "firebase"
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/database';
 
 const config = {
     apiKey: "AIzaSyDfSq_80a8Ol07LpOk-AseBZGHyQQpB10E",
@@ -10,9 +13,8 @@ const config = {
   };
 
 export const firebaseApp = firebase.initializeApp(config);
-// export const firebase = firebase
 
 // Real time events database - it allows to share events on the spot
 // We access to database and create a new section called "events"
 // Firebase will listen for any interaction with this reference and automatically update our database and our code
-export const eventsRef = firebaseApp.database().ref().child("events")
+export const eventsRef = firebase.database().ref().child("events")

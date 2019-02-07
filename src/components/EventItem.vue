469f54c8e69864060e86816cddaaca20b44b9386
Katarzyna Wietecha-Mrzyglod <k.wietecha-mrzyglod@interactivevision.pl>
2019-02-07 12:23:49 +0100

    Modal box

--- src/components/EventItem.vue
+++ src/components/EventItem.vue
@@ -12,80 +12,36 @@
           type="button"
           class="btn btn-secondary text-white m-1"
           v-if="event.email === $store.state.user.email"
-          data-toggle="modal"
-          data-target="#myModal">Modify the event</button>
-
-      <!-- The Modal -->
-      <div class="modal" id="myModal">
-        <div class="modal-dialog modal-lg">
-          <div class="modal-content">
-            <!-- Modal header -->
-            <div class="modal-header">
-              <h4 class="modal-title">Modifying component</h4>
-              <button type="button" class="close" data-dismiss="modal">&times;</button>
-            </div>
-            <!-- Modal body -->
-            <div class="modal-body">
-              <div class="form-group">
-                <label>Title</label>
-                <input
-                  type="text"
-                  placeholder="Write name of event"
-                  v-model="event.title"
-                  class="form-control" />
-              </div>
-              <div class="form-group">
-                <label>Description</label>
-                <input
-                  type="text"
-                  placeholder="Write description of event"
-                  v-model="event.description"
-                  class="form-control" />
-              </div>
-              <div class="form-group">
-                <label>Date</label>
-                <input
-                  type="date"
-                  placeholder="Write date of event"
-                  v-model="event.date"
-                  class="form-control" />
-              </div>
-              <div class="form-group">
-                <label>Location</label>
-                <input
-                  type="text"
-                  placeholder="Write location of event"
-                  v-model="event.location"
-                  class="form-control" />
-              </div>
-            </div>
-            <!-- Modal footer -->
-            <div class="modal-footer">
-              <button type="button" class="btn btn-info">Save</button>
-            </div>
-          </div>
-        </div>
-      </div>
-
+          @click="showModal">Modify</button>
       <button
         type="button"
         class="btn btn-warning text-dark m-1"
         v-if="event.email === $store.state.user.email"
-        @click="cancelEvent">Cancel the event</button>
+        @click="cancelEvent">Delete</button>
     </div>
+    <modal
+      v-show="isModalVisible"
+      @close="closeModal"
+      :method="modifyEvent"
+       />
   </div>
-
 </template>
 
 <script>
 import { eventsRef } from "../firebaseApp"
+import Modal from "./Modal.vue"
 
 export default {
+  data(){
+    return {
+      isModalVisible: false
+    }
+  },
   props: ["event"],
   methods: {
     cancelEvent(){
-      const eventTitle = this.event.title;
-
+      let eventTitle = this.event.title;
+      console.log(eventTitle);
       eventsRef.on('value', snapshot => {
         snapshot.forEach((child) => {
           child.forEach((childChild) => {
@@ -96,7 +52,20 @@ export default {
           })
         })
       })
+    },
+    showModal(){
+      this.isModalVisible = true;
+    },
+    modifyEvent(){
+      console.log(this.event.title);
+      let title = this.event.title;
+    },
+    closeModal(){
+      this.isModalVisible = false;
     }
+  },
+  components: {
+    Modal
   }
 }
 </script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,53 @@
+<template>
+  <transition name="modal-fade">
+      <div class="modal-backdrop">
+        <div class="modal"
+          role="dialog"
+          aria-labelledby="modalTitle"
+          aria-describedby="modalDescription">
+          <header
+            class="modal-header text-info"
+            id="modalTitle">
+            <slot name="header">tytul
+              <button
+                type="button"
+                class="btn-close text-info"
+                @click="close"
+                aria-label="Close modal">x</button>
+            </slot>
+          </header>
+          <section
+            class="modal-body"
+            id="modalDescription">
+            <slot name="body">
+              tekst tekst tekst
+            </slot>
+          </section>
+          <footer class="modal-footer">
+            <slot name="footer">
+              <button
+                type="button"
+                class="btn btn-info"
+                @click="close"
+                aria-label="Close modal">Save</button>
+            </slot>
+          </footer>
+        </div>
+      </div>
+    </transition>
+</template>
+
+<script>
+import { eventsRef } from "../firebaseApp"
+
+export default {
+  methods: {
+    close(){
+      this.$emit('close');
+    }
+  },
+  mounted(){
+    this.method();
+  }
+}
+</script>
